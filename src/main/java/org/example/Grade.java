348fcc2457c10e4647dbe45998fc39f14142156c
Henry <jm25002@esfe.agape.edu.sv>
2025-06-30 23:36:35 -0600

    Se creo la clase Grade.java

---
+++ src/main/java/org/example/Grade.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Grade {
+    private Student student;
+    private Course course;
+    private double score;
+
+    public Grade(Student student, Course course, double score) {
+        this.student = student;
+        this.course = course;
+        this.score = score;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return student.getName() + " - " + course.getName() + " : " + score;
+    }
+}
